c0c0943bdd9164d80510d82406c08666ef225606
b117034 <57543940+b117034@users.noreply.github.com>
2019-11-12 16:50:33 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -12,7 +12,7 @@ from starlette.staticfiles import StaticFiles
 export_file_url = 'https://drive.google.com/uc?export=download&id=1gCo3NEyo6PdpqDoDJklW1BCXPixvyT4W'
 export_file_name = 'laptop.pkl'
 
-classes = ['2', '1', '3']
+classes = ['Cultivated', 'Soil Erosion', 'Primary']
 path = Path(__file__).parent
 
 app = Starlette()
